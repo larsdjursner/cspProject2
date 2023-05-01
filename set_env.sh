08b99453b4dc0678d3ad98703f4e61fb76cdb182
larsdjursner <larsdjursner@gmail.com>
2023-05-01 14:33:34 +0200

    added plot points and removed cudf library

--- set_env.sh
+++ set_env.sh
@@ -3,10 +3,23 @@
 module load Anaconda3
 module load CUDA
 . $(conda info --base)/etc/profile.d/conda.sh
+
 conda create --name csp_numpy_cupy python=3.8
 source activate csp_numpy_cupy
-conda install -c conda-forge -c rapidsai cudf cupy cudatoolkit=10 numpy matplotlib pandas 
+# conda install -c conda-forge -c rapidsai cudf cupy cudatoolkit=10 numpy matplotlib pandas 
+conda install -c conda-forge cupy cudatoolkit=10 matplotlib numpy pandas
 conda deactivate
+
+
+# conda create --name csp_numpy_cupy 
+# source activate csp_numpy_cupy
+# conda create -n rapid -c rapidsai -c conda-forge -c nvidia rapids=23.04 python=3.10 cudatoolkit=11.2
+# conda activate rapid
+# conda install -c conda-forge -c rapidsai cudf cupy cudatoolkit=10 numpy matplotlib pandas 
+# conda install -c rapidsai -c conda-forge cudf cupy cudatoolkit=11.2 numpy matplotlib pandas 
+# conda deactivate
+
 conda init --all
 source /home/$USER/.bashrc
 
+# conda install -c rapidsai cudf -c conda-forge cupy cudatoolkit=10 numpy matplotlib pandas 
--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,12 @@ def plot(title, xlabel, ylabel, results, filename, base=2):
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
+    plt.xscale("log", base=base)
     
     for (label, size, time) in results:
-        plt.plot(size, time, label=label)
+        plt.plot(size, time, label=label, marker='o', markersize=4)
 
     plt.legend(loc="upper left")
-    plt.xscale("log", base=base)
 
     plt.savefig(filename)
     plt.clf()
