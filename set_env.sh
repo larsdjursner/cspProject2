e80a7c7a6c0ebce6600ba8000fd16b917aa3432b
larsdjursner <larsdjursner@gmail.com>
2023-05-02 17:35:05 +0200

    Added script to create new folders each run

--- set_env.sh
+++ set_env.sh
@@ -4,11 +4,13 @@ module load Anaconda3
 module load CUDA
 . $(conda info --base)/etc/profile.d/conda.sh
 
-conda create --name csp_numpy_cupy python=3.8
-source activate csp_numpy_cupy
-# conda install -c conda-forge -c rapidsai cudf cupy cudatoolkit=10 numpy matplotlib pandas 
+conda create --name cusp python=3.8
+source activate cusp
 conda install -c conda-forge cupy cudatoolkit=10 matplotlib numpy pandas
 conda deactivate
+
+conda init --all
+source /home/$USER/.bashrc
 
 
 # conda create --name csp_numpy_cupy 
@@ -19,7 +21,7 @@ conda deactivate
 # conda install -c rapidsai -c conda-forge cudf cupy cudatoolkit=11.2 numpy matplotlib pandas 
 # conda deactivate
 
-conda init --all
-source /home/$USER/.bashrc
+# conda install -c conda-forge -c rapidsai cudf cupy cudatoolkit=10 numpy matplotlib pandas 
+# conda install -c conda-forge cupy cudatoolkit=10 matplotlib numpy pandas
 
 # conda install -c rapidsai cudf -c conda-forge cupy cudatoolkit=10 numpy matplotlib pandas 
--- src/main.py
+++ src/main.py
@@ -4,16 +4,17 @@ from elementwise_mult.main import el_mult
 from array_init.main import array_init
 
 def main():
-    args = sys.argv[1:]
 
     test_cases = 10
     sizes = [2**i for i in range(1, 13)]
+    
+    # args = sys.argv[1:]
 
-    if args[0] == "test":
-        array_init(test_cases=test_cases, sizes=sizes, test=True)
-        el_mult(test_cases=test_cases, sizes=sizes, test=True)
-        matmul(test_cases=test_cases, sizes=sizes, test=True)
-        return
+    # if args[0] == "test":
+    #     array_init(test_cases=test_cases, sizes=sizes, test=True)
+    #     el_mult(test_cases=test_cases, sizes=sizes, test=True)
+    #     matmul(test_cases=test_cases, sizes=sizes, test=True)
+    #     return
     
     array_init(test_cases=test_cases, sizes=sizes)
     el_mult(test_cases=test_cases, sizes=sizes)
